4b50fc8047847625faeb3cbf19521fe15f8d6010
zsh <2905154639@qq.com>
2024-12-21 23:42:18 +0800

    VSCode use

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -10,6 +10,10 @@ int subtract(int a, int b) {
 }
 
 int main() {
+    std::cout << "5 + 3 = " << add(5, 3) << std::endl;
     std::cout << "Hello, Gradle C++!" << std::endl;
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
     return 0;
 }
